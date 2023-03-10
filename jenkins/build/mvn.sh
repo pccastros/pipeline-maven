1faf8a527f38d1aa333038ed7a7745a55c5228de
Pato <pc.castros@hotmail.com>
2023-03-10 09:21:10 -0500

    FIxed jenkins file

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -4,6 +4,6 @@ echo "******************************"
 echo "** Building JAR **************"
 echo "******************************"
 
-WORKSPACE=/var/jenkins_home/workspace/pipeline-maven
+WORKSPACE=/Users/patriciocastro/Documents/Courses/Jenkins/jenkins-data/jenkins_home/workspace/pipeline-maven
 
 docker run --rm -v $WORKSPACE/java-app:/app -v $HOME/.m2/:/root/.m2 -w /app maven:3.8.6-eclipse-temurin-11-alpine "$@"
